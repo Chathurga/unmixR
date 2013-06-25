a35d54fe9b1dfa71a9d635339333c7cba6873a95
Chathurga <chathurga@gmail.com>
2013-06-25 12:02:52 +0100

    Implement Harsanyi-Farrand-Chang (HFC) method for estimating the number of endmembers

---
+++ R/hfc.R
@@ -0,0 +1,26 @@
+hfc <- function(data, alpha) {
+  transpose <- t(data)
+  
+  dims <- dim(data)
+  nvariables <- dims[1]
+  nsamples <- dims[2]
+  
+  R <- (data %*% transpose) / nsamples
+  K <- cov(transpose)
+  
+  lcorr <- eigen(R)$values
+  lcov <- eigen(K)$values
+  
+  variance <- sqrt(2*(lcorr^2 + lcov^2) / nsamples)
+  difference <- lcorr - lcov
+  
+  p <- length(alpha)
+  vd <- array(0, p);
+  
+  for (i in 1:p) {
+      tau <- -qnorm(alpha[i], array(0, nvariables), variance)
+      vd[i] <- sum(difference > tau)
+  }
+  
+  vd
+}
