fc02a86ed2f16f65a48c2401d13ee221d9704f8e
Chathurga <chathurga@gmail.com>
2013-06-29 08:54:04 +0100

    Define and generate main package docs

---
+++ R/spec.unmixing-package.R
@@ -0,0 +1,13 @@
+##' Implements the N-FINDR and Vertex Component Analysis algorithms
+##' to recover pure component spectra and their respective concentrations
+##' from a set of measured spectra.
+##' 
+##' @name spec.unmixing-package
+##' @title Spectral Unmixing Methods
+##' @docType package
+##' @author Conor McManus
+##' 
+##' Maintainer: Conor McManus <chathurga@@gmail.com>
+##' @rdname spec.unmixing-package
+##' @keywords package
+NULL
