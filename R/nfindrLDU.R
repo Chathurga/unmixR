186e34a29f5cf6cc5249d137db00497b8311dbcf
Chathurga <chathurga@gmail.com>
2013-07-15 13:37:04 +0100

    Implement pre-process stage of nfindr LDU

--- R/nfindrLDU.R
+++ R/nfindrLDU.R
@@ -11,25 +11,26 @@ nfindrLDU <- function(data, p) {
 
   pm1 <- 1:p-1 # create a range from 1 to p minus 1
   
-  g <- array(0, p)
-  V <- array(0, p)
+  g <- matrix(0, nrow=p, ncol=p-1)
+  V <- pm1
 
   for (i in 1:p) {
-    idx <- 1:p
-    idx[p] = i
-    idx[i] = p
-    
-    simplex <- simplex[,idx]
+    # swap the i-th and p-th columns of the simplex
+    dup <- simplex
+    swaps <- 1:p
+    swaps[p] = i
+    swaps[i] = p
+    dup <- dup[,swaps]
     
     # get the partitioned components of the simplex matrix
-    A <- simplex[pm1,pm1]
-    b <- simplex[pm1,p]
-    c <- simplex[p,pm1]
-    d <- simplex[p,p]
+    A <- dup[pm1,pm1]
+    b <- dup[pm1,p]
+    c <- dup[p,pm1]
+    d <- dup[p,p]
     
-    g[i] <- t(c) * solve(A)
-    V[i] <- abs(d - (t(g[i]) * b))
+    g[i,] <- t(c) %*% solve(A)
+    V[i] <- abs(d - (t(g[i,]) %*% b))
   }
   
-  V
+  g
 }
