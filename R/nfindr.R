f18e560aa23acbcd6bfc5ded6b9c6481bac75326
Chathurga <chathurga@gmail.com>
2013-07-25 08:55:35 +0100

    More work on generic nfindr function

--- R/nfindr.R
+++ R/nfindr.R
@@ -1,4 +1,4 @@
-nfindr.default <- function(data, p, method="LDU", iters=3*p) {
+nfindr.default <- function(data, p, method="LDU", iters=3*p, drop=FALSE) {
   methods <- c("99", "LDU", "SeqLDU") # valid methods
   
   # check if the method passed in is valid
@@ -6,4 +6,31 @@ nfindr.default <- function(data, p, method="LDU", iters=3*p) {
     methodsStr <- paste(methods, collapse=", ")
     stop(paste("Invalid option for method parameter, try:", methodsStr))
   }
+  
+  # transform the input into a matrix
+  data <- as.matrix(data)
+  
+  # reduce the dimensionality of the data using PCA
+  # do nothing if the data was passed in already reduced
+  if (ncol(data) != p - 1) {
+    data <- prcomp(data)$x[,1:(p-1),drop=F]
+  }
+  
+  # select random indices that form the initial simplex
+  # this simplex will be inflated until the pure pixels are found
+  indices <- sample(nrow(data), p)
+  simplex <- matrix(0, nrow=p, ncol=p)
+  simplex[1,] <- 1
+  simplex[2:p,] <- reduced[indices,]
+  
+  # get the selected nfindr method
+  nfindrFunc <- get(paste("nfindr", method, sep=""))
+  # call the function to get the indices of the endmembers
+  indices <- nfindrFunc(data, p, iters=iters)
+  
+  # return a model
+  structure(list(
+    data = if (!drop) data else NULL,
+    indices = indices
+  ), class = "nfindr")
 }
