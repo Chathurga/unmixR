68b54f574ab8fd3623a6079cc64a7b0c46254374
Chathurga <chathurga@gmail.com>
2013-07-27 16:37:47 +0100

    Improved abundance prediction docs

--- R/predict.nfindr.R
+++ R/predict.nfindr.R
@@ -1,15 +1,22 @@
-##' @param struct The nfindr structure returned by one of the nfindr
-##'   algorithms
-##' @rdname predict #CB: suggests: @rdname nfindr
+##' Predict Endmember Abundances
+##' 
+##' Predicts the abundance precentages of each endmember at all sample points
+##' using the Non-Negative Least Squares method
+##' 
+##' @param object The N-FINDR structure returned by the general
+##'   \code{\link{nfindr}} interface
+##' @return A matrix where the abundances for an endmember are returned
+##'   column-wise. Each value is in the range \code{0 - 1}
+##' @rdname predict.nfindr
 ##' @method predict nfindr
 ##' @S3method predict nfindr
 ##' @export
 
-predict.nfindr <- function(object, ...) {  #CB: obey signature of generic, see ?predict
+predict.nfindr <- function(object, ...) {
   data <- object$data
-  endmembers <- t (data [object$indices,])
+  endmembers <- t(data[object$indices,])
   
-  t (apply (data, 1, function (spectrum) {
-    nnls (endmembers, spectrum)$x
+  t(apply(data, 1, function (spectrum) {
+    nnls(endmembers, spectrum)$x
   }))
 }
