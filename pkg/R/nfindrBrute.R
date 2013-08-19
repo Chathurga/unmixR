390e8fe06cd1c8ef1756ff3d2e6a14e7ce1294c2
Chathurga <chathurga@gmail.com>
2013-08-19 11:45:42 +0100

    Create brute force implementation of nfindr

--- pkg/R/nfindrBrute.R
+++ pkg/R/nfindrBrute.R
@@ -1,3 +1,18 @@
-nfindrBrute <- function() {
+nfindrBrute <- function(data, p) {
+  data <- as.matrix(data)
+  data <- prcomp(data)$x[, 1:(p-1), drop=FALSE]
   
+  # generate all possible unique combinations of p indexes
+  combos <- combn(nrow(data), p, simplify=TRUE)
+  n <- ncol(combos)
+  # generate the volumes of all the simplexes using the indexes
+  volumes <- sapply(1:n, function(i) {
+    idx <- combos[,i]
+    simplex <- rbind(rep(1, p), data[idx,])
+    
+    abs(det(simplex))
+  })
+  
+  col <- which.max(volumes)
+  combos[, col]
 }
