ef746e9a8989ed99dcac3f93e8aa43618f03424e
Chathurga <chathurga@gmail.com>
2013-08-22 12:41:11 +0100

    Added docs for Lopez VCA

--- pkg/R/lvca.R
+++ pkg/R/lvca.R
@@ -1,3 +1,23 @@
+##' Lopez VCA
+##' 
+##' The original VCA algorithm (\code{\link{vca}}) contains auxiliary
+##' operations (signal to noise estimation, etc.) which could be omitted while
+##' still maintaining a fully functional algorithm. In paper that describes
+##' the Modified VCA algorithm (\code{\link{mvca}}) the author provides a
+##' description of the original VCA with only the essential components
+##' included. This is an implementation of that algorithm
+##' 
+##' @param data Data to unmix. It will be converted to a matrix using
+##'   as.matrix. The matrix should contain a spectrum per row.
+##' @param p Number of endmembers
+##' @return Endmember indices
+##' 
+##' @references Lopez, S.; Horstrand, P.; Callico, G.M.; Lopez, J.F.;
+##' Sarmiento, R., "A Low-Computational-Complexity Algorithm for
+##' Hyperspectral Endmember Extraction: Modified Vertex Component Analysis,"
+##' Geoscience & Remote Sensing Letters, IEEE, vol.9 no.3 pp.502,506, May 2012
+##' doi: 10.1109/LGRS.2011.2172771
+
 lvca <- function(data, p) {
   data <- as.matrix(data)
   Y <- t(prcomp(data)$x[,1:p])
