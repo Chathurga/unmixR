ec8def6945af2c389c55b5bc8df6eeef3618a111
Chathurga <chathurga@gmail.com>
2013-08-29 17:08:02 +0100

    Add unit test for repvec.col

--- pkg/R/repvec.col.R
+++ pkg/R/repvec.col.R
@@ -10,3 +10,20 @@
 ##' @rdname repvec.col
 
 .repvec.col <- function(v, n) as.matrix(v)[, rep(1, n)]
+
+.test(.repvec.col) <- function() {
+  # test: verify functionality
+  
+  v <- c(1, 2, 3)
+  n <- 3
+  
+  expected <- matrix(c(
+    c(1,1,1),
+    c(2,2,2),
+    c(3,3,3)
+  ), ncol=3, byrow=TRUE)
+  
+  result <- .repvec.col(v, n)
+  
+  checkEquals(result, expected)
+}
