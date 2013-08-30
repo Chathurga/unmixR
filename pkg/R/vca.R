a7d8cbea94b22b6d1685d45de167c22e317243e4
Chathurga <chathurga@gmail.com>
2013-08-30 14:46:45 +0100

    Added docs for general VCA interface

--- pkg/R/vca.R
+++ pkg/R/vca.R
@@ -1,3 +1,28 @@
+##' General interface to Vertex Component Analysis spectral unmixing
+##' implementations
+##' 
+##' This algorithm is based on the geometry of convex sets. It exploits the
+##' fact that endmembers occupy the vertices of a simplex.
+##' 
+##' @param data Data to unmix. It will be converted to a matrix using
+##'   as.matrix. The matrix should contain a spectrum per row.
+##' @param p Number of endmembers
+##' @param method The N-FINDR algorithm to use. Options:
+##'   \itemize{
+##'     \item 05 (\code{\link{vca05}})
+##'     \item Lopez (\code{\link{vcaLopez}})
+##'     \item Mod (\code{\link{vcaMod}})
+##'   }
+##'   Default: Mod as it is the most efficient
+##' @param seed vca05 and vcaLopez both need to generate a random vector. Set
+##'   the random number generator seed with this
+##' 
+##' @return A structure which contains:
+##'   \itemize{
+##'     \item \strong{data}: the original data
+##'     \item \strong{indices}: the indices of the calculated endmembers
+##'   }
+
 vca <- function(data, p, method, seed) {
   UseMethod("vca")
 }
