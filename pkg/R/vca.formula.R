8010af24d13b942756912d0ee0ff4f28c4c49323
Chathurga <chathurga@gmail.com>
2013-09-18 00:29:31 +0100

    Moved vca.formula to its own file

---
+++ pkg/R/vca.formula.R
@@ -0,0 +1,8 @@
+##' @rdname vca
+##' @method vca formula
+##' @S3method vca formula
+
+vca.formula <- function(formula, frame, p, method="Mod", seed=NULL, ...) {
+  data <- model.matrix(formula, frame)
+  vca(formula, frame, p, method, seed, ...)
+}
