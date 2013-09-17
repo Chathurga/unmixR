ec269bbac194a225da08495c355fc7b4197a7fe2
Chathurga <chathurga@gmail.com>
2013-09-18 00:29:47 +0100

    Moved vca.default to its own file

--- pkg/R/vca.R
+++ pkg/R/vca.R
@@ -32,47 +32,6 @@ vca <- function(data, p, method, seed, ...) {
   UseMethod("vca")
 }
 
-##' @rdname vca
-##' @method vca default
-##' @S3method vca default
-vca.default <- function(data, p, method="Mod", seed=NULL, ...) {
-  methods <- c("05", "Lopez", "Mod")
-  
-  # check for p being with the valid range, >= 2
-  if (!is.numeric(p) || p < 2) {
-    stop("p must be a positive integer greater than 2")
-  }
-  
-  # check if the method passed in is valid
-  if (!method %in% methods) {
-    methodsStr <- paste(methods, collapse=", ")
-    stop(paste("Invalid option for method parameter, try:", methodsStr))
-  }
-  
-  # transform the input into a matrix
-  data <- as.matrix(data)
-  
-  # set the random number generator seed if supplied
-  if (!is.null(seed)) {
-    set.seed(seed)
-  }
-  
-  vcaFunc <- get(paste("vca", method, sep=""))
-  val <- vcaFunc(data, p, ...)
-  
-  if (method == "05") {
-    struct <- val
-  } else {
-    struct <- list(
-      data = data,
-      indices = sort(val)
-    )
-  }
-  
-  
-  structure(struct, class = "vca")
-}
-
 .test(vca) <- function() {
   data <- laser
   seed <- 10 # fix the seed
