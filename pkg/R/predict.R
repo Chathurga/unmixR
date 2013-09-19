7bc84a39256819c8b45158890c9a0cd77a033a97
Chathurga <chathurga@gmail.com>
2013-09-19 11:52:20 +0100

    Created predict function for VCA and merged them into one file

--- pkg/R/predict.R
+++ pkg/R/predict.R
@@ -1,10 +1,18 @@
+.predict <- function(object, newdata=object$data, ...) {
+  endmembers <- t(newdata[object$indices,])
+  
+  t(apply(newdata, 1, function(spectrum) {
+    nnls(endmembers, spectrum)$x
+  }))
+}
+
 ##' Predict Endmember Abundances
 ##' 
 ##' Predicts the abundance percentages of each endmember at all sample points
 ##' using the Non-Negative Least Squares method
 ##' 
-##' @param object The N-FINDR structure returned by the general
-##'   \code{\link{nfindr}} interface
+##' @param object The N-FINDR/VCA structure returned by the
+##'   \code{\link{nfindr}}/\code{\link{vca}} interface
 ##' @param newdata If the data stored in the object is not the data that
 ##'   should be checked for abundances then this parameter allows for passing
 ##'   in new data
@@ -12,15 +20,17 @@
 ##'   predict function
 ##' @return A matrix where the abundances for an endmember are returned
 ##'   column-wise. Each value is in the range \code{0 - 1}
-##' @rdname predict.nfindr
+##' 
+##' @name predict
+##' @rdname predict
 ##' @method predict nfindr
 ##' @S3method predict nfindr
-##' @export
 
-predict.nfindr <- function(object, newdata=object$data, ...) {
-  endmembers <- t(newdata[object$indices,])
-  
-  t(apply(newdata, 1, function(spectrum) {
-    nnls(endmembers, spectrum)$x
-  }))
-}
+predict.nfindr <- .predict
+
+##' @name predict
+##' @rdname predict
+##' @method predict vca
+##' @S3method predict vca
+
+predict.vca <- .predict
