55a7616fed6af9dcd6db225b042f53d9d1acbba6
Claudia Beleites <Claudia.Beleites@ipht-jena.de>
2013-09-26 11:44:43 +0200

    more elaborate treatment of the model formula: - make sure there is no response - silently drop intercept

--- pkg/unmixR/R/vca.formula.R
+++ pkg/unmixR/R/vca.formula.R
@@ -4,6 +4,20 @@
 ##' @S3method vca formula
 
 vca.formula <- function(formula, frame, p, method="Mod", seed=NULL, ...) {
-  data <- model.matrix(formula, frame)
+  
+  mt <- terms (formula, data = frame)
+  
+  ## response term is not allowed
+  if (attr(mt, "response") > 0L) stop("VCA models cannot have response")
+  
+  ## drop intercept silently
+  attr(mt, "intercept") <- 0L
+  
+  data <- model.matrix (mt, frame)
+  
   vca(data, p, method, seed, ...)
+}
+
+.test (vca.formula) <- function (){
+  checkException (vca (x ~ ., data.frame (x = matrix (1:4, 2)), p = 2))
 }
